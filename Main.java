9ab5f662a2a3028a92aa3f8b4aec7d2682db6e6e
TRABZI <57754442+TRABZI@users.noreply.github.com>
2021-12-07 13:58:59 +0100

    commit modifs

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, amine trabzi!");
-        System.out.println("Modifications on file Main.java");
+        System.out.println("Modifications 1 on file Main.java");
+        System.out.println("Modifications 2 on file Main.java");
     }
 }
 
